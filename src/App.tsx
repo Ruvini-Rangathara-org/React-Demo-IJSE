a3db09ef3171965efede1b2ff892f5aa84062677
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-12-16 11:39:27 +0530

    Life cyle methods

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
 // import Home from "./views/home";
+import Card from "./components/card/card";
 
-
-const arr = [1, 2, 3, "A", "B", 4]
-console.log(arr)
-
-
-//Properties vidiyta component ekata danna ona ewaprops
 interface Props {
     title: string
     content: string
@@ -18,7 +13,16 @@ interface State {
     count: number
 }
 
+const arr = [1, 2, 3, "A", "B", 4]
+console.log(arr)
+
+
 class App extends React.Component<Props, State> {
+
+
+    up = () => {
+        this.setState({count: this.state.count + 1})
+    }
 
     constructor(props: Props) {
         console.log('--constructor')
@@ -26,37 +30,6 @@ class App extends React.Component<Props, State> {
         this.state = {
             count: 0
         }
-    }
-
-    //constructor complexity eka hide karanava
-    // state = {
-    //   count: 0
-    // }
-
-
-    static getDerivedStateFromProps(props: Props, state: State) {
-        console.log('--getDerivedStateFromProps - '+props.title+" "+state.count)
-        return null
-    }
-
-    componentDidMount() {
-        console.log('--componentDidMount')
-    }
-
-    shouldComponentUpdate(nextProps: Readonly<Props>, nextState: Readonly<State>, nextContext: any): boolean {
-        console.log('--shouldComponentUpdate')
-        console.log(nextProps+" "+nextState+" "+nextContext)
-        return true;
-    }
-
-    getSnapshotBeforeUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>): any {
-        console.log('--getSnapshotBeforeUpdate')
-        console.log(prevProps+" "+prevState)
-        return null;
-    }
-
-    up = () => {
-        this.setState({count: this.state.count + 1})
     }
 
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | any | null | undefined {
@@ -69,7 +42,11 @@ class App extends React.Component<Props, State> {
                     <div className={'text-2xl font-bold'}>{this.state.count}</div>
                     <br/>
                     <button className={'bg-green-600 text-white p-5'} onClick={this.up}>Click</button>
-                </div>
+                </div>life cycle of react component
+
+            {
+                this.state.count > 2 ? null : <Card title={this.props.title} content={this.props.content}/>
+            }
 
 
                 <Footer/>
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,20 +1,71 @@
 import React from 'react';
 
+//Properties vidiyta component ekata danna ona ewaprops
 interface Props {
-  title?: string,
-  content?: string
+    title: string
+    content: string
+}
+
+interface State {
+    count: number
 }
 
 class Card extends React.Component<Props, any> {
-  render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | any | null | undefined {
-    return(
-      <div className={'w-72 p-5 border-solid border-2 m-5'}>
-        <h1 className={'text-2xl font-bold'}>{this.props.title}</h1>
-        <p className={'my-5'}>{this.props.content}</p>
-        <button className={'bg-green-600 p-2 text-white'}>Read More...</button>
-      </div>
-    );
-  }
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | Iterable<React.ReactNode> | React.ReactPortal | boolean | any | null | undefined {
+        return (
+            <div className={'w-72 p-5 border-solid border-2 m-5'}>
+                <h1 className={'text-2xl font-bold'}>{this.props.title}</h1>
+                <p className={'my-5'}>{this.props.content}</p>
+                <button className={'bg-green-600 p-2 text-white'}>Read More...</button>
+            </div>
+        );
+    }
+
+
+    constructor(props: Props) {
+        console.log('--constructor')
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+//constructor complexity eka hide karanava
+// state = {
+//   count: 0
+// }
+
+
+    static getDerivedStateFromProps(props: Props, state: State) {
+        console.log('--getDerivedStateFromProps - ' + props.title + " " + state.count)
+        return null
+    }
+
+    componentDidMount() {
+        console.log('--componentDidMount')
+    }
+
+    shouldComponentUpdate(nextProps: Readonly<Props>, nextState: Readonly<State>, nextContext: any): boolean {
+        console.log('--shouldComponentUpdate')
+        console.log(nextProps + " " + nextState + " " + nextContext)
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>): any {
+        console.log('--getSnapshotBeforeUpdate')
+        console.log(prevProps + " " + prevState)
+        return null;
+    }
+
+    componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>, snapshot?: any) {
+        console.log('--componentDidUpdate')
+        console.log(prevProps + " " + prevState + " " + snapshot)
+    }
+
+    componentWillUnmount() {
+        console.log('--componentWillUnmount')
+    }
+
 }
 
 export default Card;
